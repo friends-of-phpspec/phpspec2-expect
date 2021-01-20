817479e8b5e6704b242f7a1febf3f6dcd0d0d5ea
Jakob Linskeseder <jakob.linskeseder@karriere.at>
2021-01-20 15:33:24 +0100

    Add linting

--- src/Subject.php
+++ src/Subject.php
@@ -9,14 +9,13 @@ class Subject extends BaseSubject
     public function __call(string $method, array $arguments = array())
     {
         if (preg_match('/^(to|notTo)(.+)$/', $method, $matches)) {
-            $method = 'should'.$matches[2];
+            $method = 'should' . $matches[2];
 
             if ('notTo' === $matches[1]) {
-                $method = 'shouldNot'.$matches[2];
+                $method = 'shouldNot' . $matches[2];
             }
         }
 
         return parent::__call($method, $arguments);
     }
 }
-
